d91d4808b035118461c529f966fb3a76b71a99a1
unknown <>
2020-03-07 18:52:48 +0800

    第二次作业,修改版

--- homework2.py
+++ homework2.py
@@ -17,5 +17,7 @@ def quadratic(a,b,c):
             x1=(-b+math.sqrt(d))/(2*a)
             x2=(-b-math.sqrt(d))/(2*a)
             return x1,x2
-
-print(quadratic(1,2,1))
+a=int(input("输入a的值："))
+b=int(input("输入b的值："))
+c=int(input("输入c的值："))
+print(a,b,c)
